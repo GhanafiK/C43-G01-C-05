e388046ebaa19e98ab50873d4c5d01bdeae269aa
GHK <gamalhanafi26@gmail.com>
2024-12-23 00:20:34 +0200

    Add project files.

---
+++ C43-G01-C#05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C43_G01_C_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
