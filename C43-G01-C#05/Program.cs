72e997aae13eba3a347bfcaa0bcd329fc32c3362
GHK <gamalhanafi26@gmail.com>
2024-12-23 00:48:39 +0200

    Casting (Boxing - UnBoxing)

--- C43-G01-C#05/Program.cs
+++ C43-G01-C#05/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Casting (Boxing - UnBoxing)
+            #region Boxing [Safe Casting]
+            //object obj = new object();
+            //int x = 1;
+            //obj = x;// Boxing casting ==> convert from value type to reference type 
+            #endregion
+
+            #region unBoxing [unsafe casting]
+            //object obj2=new object();
+            //obj2 = 10;
+            //int y = (int)obj2;// unBoxing casting ==> convert from reference type to value type
+            #endregion
+
+
+            #endregion
         }
     }
 }
