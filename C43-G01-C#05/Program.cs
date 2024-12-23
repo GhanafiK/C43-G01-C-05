78f10bf22e908493c98a4b306aa09502eb6462af
GHK <gamalhanafi26@gmail.com>
2024-12-23 02:31:47 +0200

    Nullable (value type - reference type)

--- C43-G01-C#05/Program.cs
+++ C43-G01-C#05/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             #region Casting (Boxing - UnBoxing)
+
             #region Boxing [Safe Casting]
             //object obj = new object();
             //int x = 1;
@@ -17,6 +18,44 @@
             //int y = (int)obj2;// unBoxing casting ==> convert from reference type to value type
             #endregion
 
+
+            #endregion
+
+            #region Nullable (value type - reference type)
+
+            #region nullable value type
+            ////value type does n't accept null
+            //int x = 22;// only accept int numbers
+            //int? xx = null;// accept int numbers & null
+            //int y;
+            //if (xx != null)
+            //{
+            //    y = (int)xx;// casting
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+
+            ////or
+            ////we use second method because it does n't has casting
+
+            //if (xx.HasValue)
+            //{
+            //    y = xx.Value;
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+            #endregion
+
+            #region nullable reference type
+            ////reference type accept null but we use nullable with it to remove warning
+            //string name = null;//has ==> warning that name can be null
+            //string? name1 = null;//nullable ==> no warning ==> use it
+            //string name3 = null!;// null forgiveness operator (!) ==> no warning
+            #endregion
 
             #endregion
         }
